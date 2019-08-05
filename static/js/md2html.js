477900b2586237ec5d44adb10a1e8ea7c387fb57
oliwang <Dg1AvHjj>
2019-08-06 00:00:56 +0800

    finished implementing codeblock extension

--- static/js/md2html.js
+++ static/js/md2html.js
@@ -105,15 +105,61 @@ var app = new Vue({
                     regex: new RegExp(`<${key}>`, 'g'),
                     replace: `<${key} style="${styleMap[key]}">`
                 }));
+
+
+            const codeblock = {
+                type: 'output',
+                regex: /<pre>\s*<code class=\"([\w\-]+\s*[\w\-]*)\">\s*([\w\s\W]+)<\/code>\s*<\/pre>/g,
+                replace: function(d, language_name, code_content){
+                    code_content = code_content.replace(/</g, "&lt;");
+                    code_content = code_content.replace(/>/g, "&gt;");
+
+                    var $section = $('<section class="code-snippet__fix code-snippet__js"></section>');
+                    var $ul = $('<ul class="code-snippet__line-index code-snippet__js"></ul>');
+
+                    var lang_re = /(\w+) language-\1/g;
+                    var match = lang_re.exec(language_name);
+                    var language_str = "";
+                    if(match) {
+                        language_str = match[1];
+                    }
+
+                    const code_content_list = code_content.split("\n");
+
+
+                    var $pre = $('<pre class="code-snippet__js" data-lang="' +language_str+'"></pre>');
+
+                    for(let i=0; i < code_content_list.length; i++) {
+                        const $li = $('<li></li>');
+                        var str = "<br>";
+                        if(code_content_list[i] !== ""){
+                            str = code_content_list[i];
+
+                        }
+                        const $span = $('<code><span class="code-snippet_outer">'+ str +'</span></code>');
+
+                        $ul.append($li);
+                        $pre.append($span);
+                    }
+
+                    $section.append($ul);
+                    $section.append($pre);
+                    return $section[0].outerHTML;
+
+                }
+
+            };
+
             this.converter = new showdown.Converter({
-                extensions: [...bindings],
+                extensions: [...bindings, codeblock],
                 noHeaderId: true, // important to add this, else regex match doesn't work
                 // simpleLineBreaks: true
+                tables:true
             });
 
             var html = this.converter.makeHtml(this.md_editor.getValue());
-            var css_style = "<style>" + this.css_editor.getValue() + "</style>";
-            this.output = css_style+html;
+            // var css_style = "<style>" + this.css_editor.getValue() + "</style>";
+            this.output = html;
         },
         copy: function() {
             var clipboardDiv = document.getElementById('output');
@@ -142,12 +188,12 @@ var app = new Vue({
         },
         generate_style_map: function() {
             const css_raw_content = this.css_editor.getValue();
-            const css_re = /\b\s*(\w+)\s*\{(.*)\}/g;
+            const css_re = /\b\s*(\w+)\s*\{([\w\:\;\#\'\-\%\s]*)\}/g;
             match = css_re.exec(css_raw_content);
             var styleMap = {};
             while(match) {
                 // console.log(match[0], match[1], match[2]);
-                styleMap[match[1]] = match[2];
+                styleMap[match[1]] = match[2].trim();
 
                 match = css_re.exec(css_raw_content);
             }
